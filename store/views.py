eff7ff694a02258f2e9baee908b89f5ea1a52d25
kunal patel <kunno1751@gmail.com>
2025-12-05 14:04:47 +0530

    add next page button to setup move to nextpage

--- store/views.py
+++ store/views.py
@@ -3,7 +3,7 @@ from .models import Product
 from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id 
-
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
 # Create your views here.
@@ -15,26 +15,36 @@ def store(request, category_slug=None):
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
         product_count = products.count()
+        paged_products = products  
     else:
-        products = Product.objects.filter(is_available=True)  # छोटा सा simplification
-        product_count = products.count()  # ✅ spelling ठीक कर दिया
+        products = Product.objects.filter(is_available=True)  
+        paginator = Paginator(products, 3)  
+        page = request.GET.get('page')
+        try:
+            paged_products = paginator.page(page)
+        except PageNotAnInteger:
+            paged_products = paginator.page(1)
+        except EmptyPage:
+            paged_products = paginator.page(paginator.num_pages)
+        product_count = products.count()  
 
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
+
 
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
         in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
- 
-    except Exception as e:
-        raise e
+    except Product.DoesNotExist:
+        single_product = None
+        in_cart = False
     
     context = {
         'single_product': single_product,
-        'in_cart' : in_cart,
+        'in_cart': in_cart,
     }
     return render(request, 'store/product_detail.html', context)
