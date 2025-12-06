1ccad3d4aea7ddf3820859a159fe001b41383676
kunal patel <kunno1751@gmail.com>
2025-12-06 17:10:02 +0530

    fix the search button

--- store/views.py
+++ store/views.py
@@ -4,6 +4,7 @@ from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id 
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q   # ✅ Added this import
 
 
 # Create your views here.
@@ -17,7 +18,7 @@ def store(request, category_slug=None):
         product_count = products.count()
         paged_products = products  
     else:
-        products = Product.objects.filter(is_available=True)  
+        products = Product.objects.filter(is_available=True).order_by('id')
         paginator = Paginator(products, 3)  
         page = request.GET.get('page')
         try:
@@ -48,3 +49,17 @@ def product_detail(request, category_slug, product_slug):
         'in_cart': in_cart,
     }
     return render(request, 'store/product_detail.html', context)
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(
+                Q(description__icontains=keyword) | Q(product_name__icontains=keyword)
+            )
+            context = {
+                'products': products,
+                'product_count': products.count(),
+            }
+            return render(request, 'store/store.html', context)
