2f2fd6a03979f603b1374a180983ae7bbe9cd88a
kunal patel <kunno1751@gmail.com>
2025-12-16 18:05:17 +0530

    fix the error

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout as auth_logout
 from .forms import RegistrationForm
 from .models import Account
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -37,9 +38,23 @@ def register(request):
 
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            # messages.success(request, 'You are now logged in')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid login credentials')
+            return redirect('login')
     return render(request, 'accounts/login.html')
 
-
+@login_required(login_url = 'login')
 def logout(request):
     auth_logout(request)  # this clears the session
-    return redirect('login')  # redirect to login page after logout
+    messages.success(request, 'You are logged out. ')
+    return redirect('login')
