436fe27cc6b6aef9d608b0c99d0728f658063af0
kunal patel <kunno1751@gmail.com>
2026-01-03 18:23:54 +0530

    fix the payment successful page greatkart

--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from carts.models import CartItem
 from .forms import OrderForm
 import datetime
 from .models import Order, Payment, OrderProduct
 import json
 from store.models import Product
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 
 def payments(request):
     body = json.loads(request.body)
@@ -51,9 +53,20 @@ def payments(request):
 
         CartItem.objects.filter(user=request.user).delete()
 
+        mail_subject = 'Thankyou for your order'
+        message = render_to_string('accounts/order_recieved_email.html' , {
+            'user': request.user,
+            'order': order,
+        })
+        to_email = request.user.email
+        send_email = EmailMessage(mail_subject, message, to=[to_email])
+        send_email.send()
 
-
-    return render(request, 'orders/payments.html')
+        data = {
+            'order_number': order.order_number,
+            'transID': payment.payment_id,
+        }
+        return JsonResponse(data)
 
 
 
@@ -123,3 +136,8 @@ def place_order(request):
         'tax': tax,
         'grand_total': grand_total,
     })
+
+
+
+def order_complete(request):
+    return render(request, 'orders/order_complete.html')
