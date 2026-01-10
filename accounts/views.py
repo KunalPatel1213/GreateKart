5b8aba09c137635df61fe13cd19adb9058b82a47
kunal patel <kunno1751@gmail.com>
2026-01-10 13:05:38 +0530

    fix the style edit profile aur create models

--- accounts/views.py
+++ accounts/views.py
@@ -226,4 +226,11 @@ def resetPassword(request):
 
 def my_orders(request):
     orders = Order.objects.filter(user=request.user, is_ordered=True).order_by('-created_at')
+    context = {
+        'orders':orders,
+    }
     return render(request, 'accounts/my_orders.html')
+
+
+def edit_profile(request):
+    return render(request, 'accounts/edit_profile.html')
